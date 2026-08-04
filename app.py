8f40012d6e7863ff2c72b29d62c007d859a53877
isamalgraif-bit <isamalgraif@gmail.com>
2026-08-04 12:02:27 +0300

    West ERP Professional v20.11.5

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from decimal import Decimal
 import qrcode
 from openpyxl import Workbook
 
-APP_VERSION = "20.11.2"
+APP_VERSION = "20.11.5"
 
 JOURNAL_ACCOUNT_TYPES = [
     "", "عميل", "مورد", "موظف", "مندوب مبيعات", "بنك", "صندوق",
